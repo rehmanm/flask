55a055015d08e43d37388161af609e171c5882c4
rehmanm <muhammad.abdulrehman@gmail.com>
2018-08-18 02:25:17 +0500

    Adding Database

--- learning/app.py
+++ learning/app.py
@@ -4,11 +4,40 @@ from flask_moment import Moment
 from datetime import datetime
 
 from NameForm import NameForm
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 moment = Moment(app)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+print(basedir)
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
